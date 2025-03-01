4751d1cb2e54aa55b511a015f208e15f45fda2ef
hobmartin <ivanderda1999@gmail.com>
2025-03-01 19:11:44 +0200

    fix: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === "production" ? "/{repo-name}/" : "/", // Replace {repo-name} with your repository name
+  base: mode === "production" ? "/super-duper-palm-tree/" : "/",
 }));
